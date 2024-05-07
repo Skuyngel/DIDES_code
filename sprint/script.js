1739fad45b7dcfc129cec92541b2b1d743c567e3
Lukas Bucher <lukas.bucherr@gmail.com>
2024-05-07 13:43:23 +0200

    bg & btn

--- sprint/script.js
+++ sprint/script.js
@@ -1,11 +1,14 @@
 let currentStar = 0;
 let interval;
 let stars = document.getElementsByClassName("star");
+
 document.getElementById("holdbutton").addEventListener('touchstart', function(){
     interval = setInterval(fillStar, 500);
+    document.getElementById("background").style.background = "radial-gradient(72.61% 62.61% at 50% 50%, #F7C8FF 0%, #9E87DF 100%)";
+    document.getElementById("holdbutton").style.animation = "shrink 0.5s infinite";
 });
 document.getElementById("holdbutton").addEventListener('touchend', function(){
-    //clearInterval(interval);
+    clearInterval(interval);
     if(currentStar >= stars.length) {
         document.getElementById("background").style.background = "radial-gradient(72.61% 62.61% at 50% 50%, rgb(79 79 79) 0%, rgb(39, 39, 39) 100%)"
         document.getElementById("content").style.background = "url(Untitled_Artwork.png)"
@@ -18,7 +21,8 @@ function fillStar(){
         clearInterval(interval)
         //five stars filled
     } else {
-        stars[currentStar].style.fill = "#B9DFB8";
+        stars[currentStar].style.fill = "#B9DFB8"; //#EAFFEA
+        stars[currentStar].style.filter = "drop-shadow(0px 0px 20px white)";
         currentStar++;
     }
 }
